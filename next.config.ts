1f41cf287974cc50ece54fade330d00c28942c27
hennkar <ole.katzenberger@gmail.com>
2025-03-23 11:59:16 +0100

    Prepare deployment to pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: '/tech-tree',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
